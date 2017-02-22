cb56881b8e423535e86fb77b54e460373628f53d
A-Little-Ant <iosuyuping@163.com>
2017-02-22 17:20:40 +0800

    添加项目文件。

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+# print   input()   
+a = 25
+b = 30
+c = (a + b) / (a - b)
+print ('c = ',c)
+#练习001
+#简单的print语句
+
+print ('#--->the code here is \'d = inpput()\'')
+d = input() #输入字符串
+print ('what you input is :',d)
+print ('#---->or you can use this \'e = input(\'please input a \string\')\'')
+e = input('please input a string: ')
+print ('what you input is:e =',e)
