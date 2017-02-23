69ce4a1a912ddec67bf224e8ac91279bb9f70584
A-Little-Ant <iosuyuping@163.com>
2017-02-23 11:46:30 +0800

    20170223联系新加

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,15 +1,28 @@
 # coding=utf-8
-# print   input()   
-a = 25
-b = 30
-c = (a + b) / (a - b)
-print ('c = ',c)
+# print   input()
+#=================   
+#a = 25
+#b = 30
+#c = (a + b) / (a - b)
+#print ('c = ',c)
+#=================
 #练习001
 #简单的print语句
-
-print ('#--->the code here is \'d = inpput()\'')
-d = input() #输入字符串
-print ('what you input is :',d)
-print ('#---->or you can use this \'e = input(\'please input a \string\')\'')
-e = input('please input a string: ')
-print ('what you input is:e =',e)
+#d = input() #输入字符串
+#print ('what you input is :',d)
+#print ('#---->or you can use this \'e = input(\'please input a \string\')\'')
+#e = input('please input a string: ')
+#print ('what you input is:e =',e)
+#=================
+#用print输出 1024*768=?
+#print ('1024 * 768 = ',1024*768)
+#=================
+#print absoluta value of an integer
+#f = input()
+#f = int(f)#此处如果不加int(),会报错提示str()->int()错误
+##f = -3000
+#if f > 0:
+#    print (f)
+#else:
+#    print (-f)
+#=================
